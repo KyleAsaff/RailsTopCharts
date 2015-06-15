ba1cc67cb87b2f77be09dedc231eff974a9f2964
Kyle Asaff <KyleAsaff@gmail.com>
2015-06-14 23:25:58 -0300

    Page Styling added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#paid'
-
-  get 'static_pages/free'
-
-  get 'static_pages/top_grossing'
+  get 'free' => 'static_pages#free'
+  get 'paid' => 'static_pages#paid'
+  get 'top_grossing' => 'static_pages#top_grossing'
 end
