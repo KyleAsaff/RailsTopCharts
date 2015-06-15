550246722e4f284733d02fd2f8ff0411879d305b
Kyle Asaff <KyleAsaff@gmail.com>
2015-06-15 02:23:55 -0300

    Created Listing Queries

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,22 @@
 class StaticPagesController < ApplicationController
   def paid
+   @static_pages = App
+   .where('price > 0.00')
+   .paginate(:page => params[:page], :per_page => 50)
+   .order('iphone_downloads + ipad_downloads')
   end
 
   def free
+   @static_pages = App
+   .where('price = 0.00')
+   .paginate(:page => params[:page], :per_page => 50)
+   .order('iphone_downloads + ipad_downloads')
   end
 
   def top_grossing
+   @static_pages = App
+   .paginate(:page => params[:page], :per_page => 50)
+   .order('iphone_revenue + ipad_revenue')
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,25 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "App Store Top List"
+    base_title = "App Store"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
   end
+
+def nav_link(link_text, link_path)
+  if current_page?('/') && link_text == 'Paid'
+  	class_name = 'active'
+  	content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+  end
+  else
+  	class_name = current_page?(link_path) ? 'active' : ''
+  	content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+  	end
+  end
+end
 end
