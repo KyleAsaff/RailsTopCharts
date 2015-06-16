58fcf1621ec4ef60c94b19c09b939a46f0025a6a
Kyle Asaff <KyleAsaff@gmail.com>
2015-06-16 16:48:51 -0300

    Fixed App ordering

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -3,19 +3,19 @@ class AppsController < ApplicationController
    @apps = App
    .where('price > 0.00')
    .paginate(:page => params[:page], :per_page => 50)
-   .order('iphone_downloads + ipad_downloads')
+   .order('(iphone_downloads + ipad_downloads) DESC')
   end
 
   def free
    @apps = App
    .where('price = 0.00')
    .paginate(:page => params[:page], :per_page => 50)
-   .order('iphone_downloads + ipad_downloads')
+   .order('(iphone_downloads + ipad_downloads) DESC')
   end
 
   def top_grossing
    @apps = App
    .paginate(:page => params[:page], :per_page => 50)
-   .order('iphone_revenue + ipad_revenue')
+   .order('(iphone_revenue + ipad_revenue) DESC')
   end
 end
